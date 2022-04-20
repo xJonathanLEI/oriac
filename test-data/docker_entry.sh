c8348181d746c29ecf30bcae348392555ad3c4a5
Jonathan LEI <me@xjonathan.dev>
2022-04-20 16:03:58 +0000

    chore: update cairo-compile to 0.8.1

--- test-data/docker_entry.sh
+++ test-data/docker_entry.sh
@@ -4,12 +4,9 @@ set -x
 set -e
 
 compile () {
-  cairo-compile $1 | jq > $2
+  cairo-compile $1 > $2
   chown $USER_ID:$GROUP_ID $2
 }
-
-apt-get update
-apt-get install -y jq
 
 compile "/contracts/run_past_end.cairo" "/artifacts/run_past_end.json"
 compile "/contracts/bad_stop_ptr.cairo" "/artifacts/bad_stop_ptr.json"
--- test-data/generate_artifacts.sh
+++ test-data/generate_artifacts.sh
@@ -2,11 +2,19 @@
 
 # Deterministically generate contract artifacts
 
-sudo docker run -it --rm \
+docker run -it --rm \
     -v "$(pwd)/artifacts:/artifacts" \
     -v "$(pwd)/contracts:/contracts:ro" \
     -v "$(pwd)/docker_entry.sh:/entry.sh:ro" \
     --env "USER_ID=$(id -u)" \
     --env "GROUP_ID=$(id -g)" \
     --entrypoint "/entry.sh" \
-    shardlabs/cairo-cli:0.7.1
+    shardlabs/cairo-cli:0.8.1
+
+# Using prettier instead of `jq` due to known issue:
+#   https://github.com/xJonathanLEI/starknet-rs/issues/76#issuecomment-1058153538
+docker run -it --rm \
+    -v "$(pwd)/artifacts:/work" \
+    --user root \
+    tmknom/prettier:2.6.2 \
+    --write .
